2c3e5824e0b6dc5d8f75fd31aa13b466dd54ab11
godofrage@gmail.com <godofrage@gmail.com@ba9d89ee-c9ea-f178-a8ae-4e93fc493209>
2011-10-21 15:01:23 +0000

    AudioManager now working on the LAN. Modified receiver so that it won't play streams coming from the same computer.

--- trunk/AudioManager/src/teletutor/audiomanager/Activator.java
+++ trunk/AudioManager/src/teletutor/audiomanager/Activator.java
@@ -5,58 +5,92 @@
 package teletutor.audiomanager;
 
 import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 import teletutor.audiomanager.impl.LANAudioManager;
 import teletutor.audiomanager.services.AudioManager;
+import teletutor.core.services.TeleChannel;
 
 /**
  *
  * @author Sabin Timalsena
  */
 public class Activator implements BundleActivator {
+
     public static BundleContext bc = null;
-    
     private AudioManager audioManager;
+    ServiceTracker channelTracker = null;
 
     @Override
-    public void start(BundleContext bc) throws Exception {
-        Activator.bc = bc;
-        
-        // fix the classloader
-//        ClassLoader bundleLoader = getClass().getClassLoader();
-//        ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
-//        Thread.currentThread().setContextClassLoader(bundleLoader);
-        
-        // create and register the AudioManager service so it can be used by 
-        // other bundles
-        audioManager = new LANAudioManager("AudioManager", null, "224.144.251.104", 22200);
-        audioManager.initialize();
-        
-        // Initially, no voice should be sent from the audiomanager;
-        // should wait till instructed by the ClassroomManager
-        //audioManager.resume();
-        
-        // reset the classloader
-        //Thread.currentThread().setContextClassLoader(oldLoader);
-        
-        bc.registerService(AudioManager.class.getName(), audioManager, new Hashtable());
+    public void start(BundleContext bcon) throws Exception {
+        Activator.bc = bcon;
+
+        // tracker for the TeleChannel service
+        // tracker for the AudioManager service not necessary
+        ServiceTrackerCustomizer customizer = new ServiceTrackerCustomizer() {
+
+            public Object addingService(ServiceReference reference) {
+                Logger.getLogger(Activator.class.getName()).log(Level.INFO, "Found TeleChannel");
+
+                TeleChannel channel = (TeleChannel) bc.getService(reference);
+                startAudio(channel);
+                return channel;
+            }
+
+            public void modifiedService(ServiceReference reference, Object serviceObject) {
+                TeleChannel channel = (TeleChannel) bc.getService(reference);
+                stopAudio();
+                startAudio(channel);
+            }
+
+            public void removedService(ServiceReference reference, Object serviceObject) {
+                stopAudio();
+            }
+        };
+
+        channelTracker = new ServiceTracker(bc, TeleChannel.class.getName(), customizer);
+        channelTracker.open();
     }
 
     @Override
     public void stop(BundleContext bc) throws Exception {
         Activator.bc = null;
-        
-        // fix the classloader
+        stopAudio();
+    }
+
+    public void startAudio(TeleChannel chan) {
+        try {
+            audioManager = new LANAudioManager("AudioManager", chan, "224.144.251.104", 22200, chan.getChannelName());
+            audioManager.initialize();
+            
+            if (chan.getTutorName().equals(chan.getChannelName())) {
+                // this is the tutor, so start the audio
+                audioManager.resume();
+            }
+            
+            bc.registerService(AudioManager.class.getName(), audioManager, new Hashtable());
+        } catch (Exception ex) {
+            Logger.getLogger(Activator.class.getName()).log(Level.SEVERE, "Could not initialize audio: ", ex);
+        }
+    }
+
+    public void stopAudio() {
+        if (audioManager == null) {
+            return;
+        }
+        audioManager.stop();
+    }
+}
+
+
+//        fix the classloader
 //        ClassLoader bundleLoader = Activator.class.getClassLoader();
 //        ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
-//        Thread.currentThread().setContextClassLoader(bundleLoader);
-//        
-        audioManager.stop();
-        // manual unregistration of services not necessary
-
-        // reset the classloader
+//        Thread.currentThread().setContextClassLoader(bundleLoader);        
+//        reset the classloader
 //        Thread.currentThread().setContextClassLoader(oldLoader);
-    }
-    
-}
